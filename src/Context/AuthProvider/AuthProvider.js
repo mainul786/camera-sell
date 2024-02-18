202b10e2e535217e1cc27e560a5087081017de84
mainul96 <110577655+mainul96@users.noreply.github.com>
2024-02-18 21:10:20 +0530

    google sign method implemetation

--- src/Context/AuthProvider/AuthProvider.js
+++ src/Context/AuthProvider/AuthProvider.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { createContext } from 'react';
 import app from './../../firebase/firebase.config';
-import {createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword} from 'firebase/auth';
+import {GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup} from 'firebase/auth';
 
 
 export const AuthContext = createContext();
 const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
 const AuthProvider = ({children}) => {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
@@ -20,6 +21,15 @@ const loginUser = (email, password) =>{
     return signInWithEmailAndPassword(auth, email, password)
 }
 
+const signInWithGoogle = () =>{
+    setLoading(true)
+    return signInWithPopup(auth, googleProvider)
+}
+
+const updateProfile = () =>{
+    
+}
+
 useEffect(()=>{
 const unsubscribe = onAuthStateChanged(auth, currentUser =>{
         setUser(currentUser);
@@ -31,6 +41,7 @@ const unsubscribe = onAuthStateChanged(auth, currentUser =>{
     const authInfo ={
         createUser,
         loginUser,
+        signInWithGoogle,
         user,
         loading
     }
